3d099d70785b36102f42b107f246f2b0529cecbc
Sujal Singh <email.sujalsingh@gmail.com>
2023-04-02 20:43:15 +0530

    added sever defaults

---
+++ src/server/defaults.py
@@ -0,0 +1,7 @@
+HOST = "0.0.0.0"
+HOST2 = "0.0.0.0"
+PORT = 54321
+GET_INTERVAL = 5  # How often will the client check for outages.
+POST_INTERVAL = 10  # How often will the TUI update.
+SHUTDOWN_DELAY = 4  # How many minutes should the client wait before shutting down after detecting an outage.
+NO_TUI = False
