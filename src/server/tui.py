7606f3f53087d466a382c8c8f92c677f2ab2c4a2
Sujal Singh <email.sujalsingh@gmail.com>
2023-04-03 21:58:57 +0530

    debug temporary ui

--- src/server/tui.py
+++ src/server/tui.py
@@ -4,26 +4,22 @@ from textual.reactive import reactive
 
 
 class Temporary(Static):
-    shared_data = reactive({})
-
     def __init__(self, shared_data: dict):
         super().__init__()
-        self._shared_data = shared_data
-        self.set_interval(10, self.update_shared_data)
+        self.shared_data = shared_data
+        self.set_interval(5, self.update_shared_data)
 
     def update_shared_data(self):
-        self.shared_data = str(self._shared_data)
+        self.shared_data = str(self.shared_data)
 
     def watch_stats(self):
         self.update(str(self.shared_data))
 
 
 class TUI(App):
-    shared_data = reactive({})
-
     def __init__(self, shared_data: dict):
         super().__init__()
-        self._shared_data = shared_data
+        self.shared_data = shared_data
 
     def compose(self) -> ComposeResult:
-        yield Temporary(self._shared_data)
+        yield Temporary(self.shared_data)
